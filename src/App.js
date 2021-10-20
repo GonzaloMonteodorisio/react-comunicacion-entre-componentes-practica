70a516866cdc3158cfbe92d384d7353900ca51d1
GonzaloMonteodorisio <gonzalomonteodorisio@gmail.com>
2021-10-20 17:10:25 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Comentario from './components/Comentario'
+import Saludo from './components/Saludo'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+        <h1>Proyecto desde cero</h1>
+        <Saludo persona='Ignacio' edad={30} />
+        <Saludo persona='Pedro' edad={28} />
+        <Saludo persona='Juan' edad={33} />
+        <hr />
+        <h3>Cajita de comentarios</h3>
+        <Comentario 
+            urlImagen="https://picsum.photos/64"
+            persona="Ignacio"
+            texto="wwwwwwwwwwwwwwwwwwwwwww"
+        />
+        <Comentario 
+            urlImagen="https://picsum.photos/64"
+            persona="Pedro"
+            texto="nnnnnnnnnnnnnnnnnnnnnnnnnn"       
+        />
+        <Comentario 
+            urlImagen="https://picsum.photos/64"
+            persona="Juan"
+            texto="yyyyyyyyyyyyyyyyyyyyyyyyyyy"        
+        />
     </div>
   );
 }
---
+++ src/components/Imagen.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Imagen({urlImagen}) {
+    return (
+        <img src={urlImagen} alt="Imagen aleatoria" className="mr-3" />
+    )
+}
